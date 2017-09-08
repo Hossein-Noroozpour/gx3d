03e25ed124455fba08a6da72a07c5104fd1ae70c
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-09-08 13:03:16 +0430

    adapted to new buffer management design

--- gx3d.py
+++ gx3d.py
@@ -59,7 +59,7 @@ class Gearoenix:
     STRING_DYNAMIC_PARTED = 'dynamic-parted'
     STRING_CUTOFF = "cutoff"
     STRING_TRANSPARENT = "transparent"
-    STRING_VERTICES_INDICES_BUFFER_SIZE = "vertices-indices-size"
+    STRING_MORPH_MESHES_BUFFER_SIZE = "morph-meshes-size"
     STRING_UNIFORM_BUFFER_SIZE = "uniform-size"
     STRING_ENGINE_SDK_VAR_NAME = 'VULKUST_SDK'
     STRING_VULKAN_SDK_VAR_NAME = 'VULKAN_SDK'
@@ -642,7 +642,7 @@ class Gearoenix:
             if len(cameras) < 1:
                 cls.show("At least one camera must exist.")
             cls.out.write(cls.TYPE_COUNT(int(
-                scene[cls.STRING_VERTICES_INDICES_BUFFER_SIZE])))
+                scene[cls.STRING_MORPH_MESHES_BUFFER_SIZE])))
             cls.out.write(cls.TYPE_COUNT(int(
                 scene[cls.STRING_UNIFORM_BUFFER_SIZE])))
             cls.out.write(cls.TYPE_COUNT(len(cameras)))
